7c507f37dc362a72554cccd4408218ac0e2c27e9
anvdmal <alekseeva.ann04@mail.ru>
2024-10-27 12:50:17 +0300

    init

---
+++ src/main/java/com/DressRental/DressRentalApplication.java
@@ -0,0 +1,11 @@
+package com.DressRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressRentalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DressRentalApplication.class, args);
+    }
+}
