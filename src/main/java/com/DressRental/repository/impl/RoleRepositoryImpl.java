48eccc5c323a04d84a2ef6e75b722755c741c089
anvdmal <alekseeva.ann04@mail.ru>
2024-11-04 20:23:27 +0300

    add annotation @Repository

--- src/main/java/com/DressRental/repository/impl/RoleRepositoryImpl.java
+++ src/main/java/com/DressRental/repository/impl/RoleRepositoryImpl.java
@@ -3,9 +3,11 @@ package com.DressRental.repository.impl;
 import com.DressRental.entity.Role;
 import com.DressRental.repository.BaseRepository;
 import com.DressRental.repository.RoleRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public class RoleRepositoryImpl extends BaseRepository<Role, UUID> implements RoleRepository {
     public RoleRepositoryImpl() {
         super(Role.class);
