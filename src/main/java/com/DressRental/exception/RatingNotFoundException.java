8c62f82ba71b7c604a18aa958d19e1d0e36ebaed
anvdmal <alekseeva.ann04@mail.ru>
2024-12-16 11:35:41 +0300

    remove rating not found exception

--- src/main/java/com/DressRental/exception/RatingNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.DressRental.exception;
-
-public class RatingNotFoundException extends RuntimeException {
-    public RatingNotFoundException() {
-        super("Пользователь не найден!");
-    }
-}
