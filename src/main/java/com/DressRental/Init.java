21a74238d33ebaa30d8c1eb5bb186c5ea6f1688f
anvdmal <alekseeva.ann04@mail.ru>
2024-12-23 11:26:07 +0300

    init categories, sizes, statuses

--- src/main/java/com/DressRental/Init.java
+++ src/main/java/com/DressRental/Init.java
@@ -1,10 +1,8 @@
 package com.DressRental;
 
-import com.DressRental.models.entities.Role;
-import com.DressRental.models.entities.User;
+import com.DressRental.models.entities.*;
 import com.DressRental.models.enums.UserRoles;
-import com.DressRental.repository.impl.RoleRepositoryImpl;
-import com.DressRental.repository.impl.UserRepositoryImpl;
+import com.DressRental.repository.impl.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,12 +13,18 @@ import java.util.List;
 @Component
 public class Init implements CommandLineRunner {
     private final UserRepositoryImpl userRepository;
+    private final DressSizeRepositoryImpl dressSizeRepository;
+    private final DressCategoryRepositoryImpl dressCategoryRepository;
+    private final StatusRepositoryImpl statusRepository;
     private final RoleRepositoryImpl roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final String defaultPassword;
 
-    public Init(UserRepositoryImpl userRepository, RoleRepositoryImpl roleRepository, PasswordEncoder passwordEncoder, @Value("${app.default.password}") String defaultPassword) {
+    public Init(UserRepositoryImpl userRepository, DressSizeRepositoryImpl dressSizeRepository, DressCategoryRepositoryImpl dressCategoryRepository, StatusRepositoryImpl statusRepository, RoleRepositoryImpl roleRepository, PasswordEncoder passwordEncoder, @Value("${app.default.password}") String defaultPassword) {
         this.userRepository = userRepository;
+        this.dressSizeRepository = dressSizeRepository;
+        this.dressCategoryRepository = dressCategoryRepository;
+        this.statusRepository = statusRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
         this.defaultPassword = defaultPassword;
@@ -30,6 +34,9 @@ public class Init implements CommandLineRunner {
     public void run(String... args) throws Exception {
         initRoles();
         initUsers();
+        initCategories();
+        initSizes();
+        initStatuses();
     }
 
     private void initRoles() {
@@ -45,6 +52,24 @@ public class Init implements CommandLineRunner {
         if (userRepository.count() == 0) {
             initAdmin();
             initClient();
+        }
+    }
+
+    private void initCategories() {
+        if (dressCategoryRepository.count() == 0) {
+            initDressCategory();
+        }
+    }
+
+    private void initSizes() {
+        if (dressSizeRepository.count() == 0) {
+            initDressSize();
+        }
+    }
+
+    private void initStatuses() {
+        if (statusRepository.count() == 0) {
+            initStatus();
         }
     }
 
@@ -66,5 +91,30 @@ public class Init implements CommandLineRunner {
         clientUser.setRoles(List.of(userRole));
 
         userRepository.create(clientUser);
+    }
+
+    private void initDressSize() {
+        List<String> sizes = List.of("XS", "S", "M", "L", "XL", "XXL");
+
+        for (String sizeName : sizes) {
+            dressSizeRepository.create(new DressSize(sizeName));
+        }
+    }
+
+    private void initDressCategory() {
+        List<String> categories = List.of("Свадебные платья", "Вечерние платья", "Выпускные платья", "Коктейльные платья", "Бальные платья",
+                "Будуарные платья", "Платья для фотосессии", "Платья для беременных");
+
+        for (String categoryName : categories) {
+            dressCategoryRepository.create(new DressCategory(categoryName));
+        }
+    }
+
+    private void initStatus() {
+        List<String> statuses = List.of("Ожидает примерки", "Активно", "Завершено", "Отменено");
+
+        for (String status : statuses) {
+            statusRepository.create(new Status(status));
+        }
     }
 }
